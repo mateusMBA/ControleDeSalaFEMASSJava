be1032499e7b183fab65e47fd45e40a0d978c6dc
Mateus Almeida <mateus.barros.almeida@gmail.com>
2025-03-11 21:18:01 -0300

    commit inicial projeto

---
+++ src/main/java/br/com/femass/ds1/ControledeSalaFEMASSJava/ControledeSalaFemassJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.femass.ds1.ControledeSalaFEMASSJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeSalaFemassJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledeSalaFemassJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/femass/ds1/ControledeSalaFEMASSJava/ControledeSalaFemassJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.femass.ds1.ControledeSalaFEMASSJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeSalaFemassJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
